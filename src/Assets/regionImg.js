32f85f96fef8e860466a9a75f07bf829033970cb
llky <lesterlohky@gmail.com>
2022-11-19 18:14:46 +0800

    Update regionImg.js

--- src/Assets/regionImg.js
+++ src/Assets/regionImg.js
@@ -1,13 +1,15 @@
 const regionImg = {
   kanto:
     "https://archives.bulbagarden.net/media/upload/thumb/2/25/LGPE_Kanto_Map.png/800px-LGPE_Kanto_Map.png",
-  johto: "https://archives.bulbagarden.net/media/upload/6/64/JohtoMap.png",
+  johto:
+    "https://archives.bulbagarden.net/media/upload/thumb/6/64/JohtoMap.png/800px-JohtoMap.png",
   hoenn: "https://archives.bulbagarden.net/media/upload/8/85/Hoenn_ORAS.png",
   sinnoh:
-    "https://archives.bulbagarden.net/media/upload/0/08/Sinnoh_BDSP_artwork.png",
+    "https://archives.bulbagarden.net/media/upload/thumb/0/08/Sinnoh_BDSP_artwork.png/800px-Sinnoh_BDSP_artwork.png",
   unova:
     "https://archives.bulbagarden.net/media/upload/f/fc/Unova_B2W2_alt.png",
-  kalos: "https://archives.bulbagarden.net/media/upload/8/8a/Kalos_alt.png",
+  kalos:
+    "https://archives.bulbagarden.net/media/upload/thumb/8/8a/Kalos_alt.png/800px-Kalos_alt.png",
   alola:
     "https://archives.bulbagarden.net/media/upload/0/0b/Alola_USUM_artwork.png",
   galar: "https://images.nintendolife.com/0cb4cd2bf797b/1280x720.jpg",
